9221e64a42a0df276ae477276ac71f41d39b2044
rezaroshan100178 <rezaroshan100178@gmail.com>
2025-12-12 03:16:58 +0330

    try searchable types by change index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import roshanexpress from "./lib/roshanExpress";
 import "./extensions/req";
 import "./extensions/res";
-import "./lib/roshanExpress";
 
 export default roshanexpress;
 
+export * from "./lib/roshanExpress";
+export * from "./extensions/req";
+export * from "./extensions/res";
